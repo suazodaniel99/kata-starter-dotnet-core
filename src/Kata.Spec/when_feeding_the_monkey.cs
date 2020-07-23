b54f459c9f9ce7a54e7d9da1225311972c1120da
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-07-23 15:41:51 -0600

    Ping

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Machine.Specifications;
 
 namespace Kata.Spec
@@ -92,6 +93,17 @@ namespace Kata.Spec
     }
     
     // Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
+    public class when_input_is_has_a_negative_number
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = Catch.Exception(()=> _systemUnderTest.Add("1,-2,3")) ; };
+
+        It should_return_the_sum_of_all_numbers = () => { _result.Message.Should().Be("negatives not allowed: -2"); };
+        static Calculator _systemUnderTest;
+        static Exception _result;
+    }
+    
     // Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
     // Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
     // Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -12,7 +12,18 @@ namespace Kata
             {
                 return 0;                
             }
-            var numbers = input.Split(new [] {",", "\n"}, StringSplitOptions.None).Select(int.Parse);
+
+            var separator = new [] {",", "\n"};
+
+            var content = input;
+            if (input.StartsWith("//"))
+            {
+                var parts = input.Split("\n");
+                content = parts[1];
+                separator = new[] {parts[0].Replace("//", "")};
+            }
+            
+            var numbers = content.Split(separator, StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
         }
     }
